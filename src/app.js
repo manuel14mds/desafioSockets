3b4023c08443a4e23327cd5b93ff147658a7e726
manuel14mds <manuel14mds@gmail.com>
2022-08-02 23:58:23 -0500

    chat completed

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import {Server} from 'socket.io'
 import viewsRouter from './routes/views.router.js'
 import productRouter from './routes/product.routes.js'
 import UserManager from './managers/user.manager.js'
+import ProductManager from './managers/product.manager.js'
 
 const app = express()
 const server = app.listen(8080, ()=>console.log('listening on 8080 port \n'))
@@ -19,17 +20,24 @@ app.use(express.json())
 app.use(express.static(__dirname+'/public'))
 
 app.use('/', viewsRouter)
-app.use('/api', productRouter)
+//app.use('/api', productRouter)
 
 const userService = new UserManager()
+const productService = new ProductManager()
 let log = []
-io.on('connection', socket=>{
+io.on('connection', async (socket)=>{
     console.log('Socket connected')
     socket.broadcast.emit('newUser')
+
+    let products = await productService.getAllProducts()
+    
+    
     socket.on('message',(data)=>{
         let currentTime = new Date();
         data.date=currentTime.toLocaleTimeString();
-        userService.addUsers(data)
+        log.push(data)
         io.emit('log',log)
     })
+    
+    app.render('home', { products, listVoid:products.length==0})
 })
--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -3,73 +3,24 @@ const socket = io({
     autoConnect: false//para que no se autoconecte el socket
 });
 
-let form = document.getElementById('addProduct')
-form.addEventListener('submit', async(e)=>{
-    e.preventDefault()
-    let data = new FormData(form)
-    let obj={}
-    data.forEach((value,key) => obj[key]=value);
-    console.log('formData: ',data )
-    console.log('obj: ',obj)
-    fetch('api/newProduct',{
-        method:'POST',
-        body:JSON.stringify(data),
-        header:{
-            'Content-Type': 'application/json'
-        }
-
-    }).then(result=>result.json())
-    .then(json=>console.log(json))
-    .finally(console.log(obj))
-})
-/* let productName = document.getElementById('name')
-let price = document.getElementById('price')
-let img = document.getElementById('img')
-
-const btn = document.getElementById('btn')
-btn.addEventListener('click', (e)=>{
-    e.preventDefault()
-    console.log('entras')
-    let product={
-        name:productName.value, 
-        price:price.value, 
-        img:img.value
-    }
-    console.log(JSON.stringify(product))
-    fetch('/api/newProduct', {
-        method:'POST',
-        body:JSON.stringify(product),
-        header:{
-            "Content-Type": "application/json",
-        }
-    }).then(result=>result.json())
-    .then(json=>console.log(json))
-    .finally(
-        productName.value='',
-        price.value='',
-        img.value=''
-    )
-}) */
-
-
-let userEmail
-function chatSocket() {
+let userName
+function chatSocket(message) {
     if (chatBox.value.trim().length > 0) {
-        console.log('entra a enviar chat')
-        socket.emit('message', { userEmail: userEmail, message: chatBox.value })
+        socket.emit('message', { userName: userName, message: message })
         chatBox.value = ''
+        console.log(message)
     }
-
 }
+
 function chatsHTML(chatList) {
     let str = ''
 
     for (const chat of chatList) {
-        if (chat.userEmail === userEmail) {
+        if (chat.userName === userName) {
             str += `
             <div>
                 <span class="u2 chat">
-                    <span class="userSay">${chat.userEmail}:</span><br>
+                    <span class="userSay">${chat.userName}:</span><br>
                     ${chat.message}<br>
                     <span class="date">${chat.date}</span>
                 </span>
@@ -79,7 +30,7 @@ function chatsHTML(chatList) {
             str += `
                 <div>
                 <span class="u1 chat">
-                    <span class="userSay">${chat.userEmail}:</span><br>
+                    <span class="userSay">${chat.userName}:</span><br>
                     ${chat.message}<br>
                     <span class="date">${chat.date}</span>
                 </span>
@@ -90,44 +41,50 @@ function chatsHTML(chatList) {
     return str
 }
 
+
+const header = document.getElementById('header')
+
 Swal.fire({
-    title: 'Ingresa tu correo',
+    title: 'Username',
     input: 'text',
-    text: 'Correo con que te identificarás',
+    text: 'Type your username',
     inputValidator: (value => {
-        return !value && 'Ingresa tu direccion de e-mail'
+        return !value && 'Type your username to indentificate you'
     }),
     allowOutsideClick: false,
     allowEscapeKey: false
 }).then(result => {
-    userEmail = result.value
+    userName = result.value
     socket.connect()//se autoconecta el socket despues de las validaciones que haga
-    header.innerText = userEmail
+    header.innerText = userName
 })
 
-const header = document.getElementById('header')
-
+//sends a new chat message
 const chatBox = document.getElementById('chatBox')
 chatBox.addEventListener('keyup', (evt) => {
     if (evt.key === 'Enter') {
-        chatSocket()
+        chatSocket(chatBox.value)
     }
 })
 
+//sends a new chat message
 const sendButton = document.getElementById('send')
 sendButton.addEventListener('click', () => {
     console.log('entra al boton enviar')
-    chatSocket()
+    chatSocket(chatBox.value)
 })
 
+//It shows the chat messages
 socket.on('log', (data) => {
     const chats = document.getElementById('chats')
     chats.innerText = ''
     chats.innerHTML = chatsHTML(data)
     chats.scrollTop = chats.scrollHeight
 })
+
+// It shows a toast when a new user is connected
 socket.on('newUser', (data)=>{
-    if(userEmail){
+    if(userName){
         Swal.fire({
             text:'Nuevo ususario en el chat',
             toast:true, 
